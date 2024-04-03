2189601aba2d930de978761adb77acb24caae65e
camyacodes <camyacodes@gmail.com>
2024-04-03 18:56:57 -0400

    added img to readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Header = () => (
   <header className="text-center py-3">
     <h1>Templify</h1>
-    <h4>Email Template Customizer</h4>
+    <h4 className='text-muted'>Email Template Generator</h4>
   </header>
 );
 
