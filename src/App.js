1e3872889c6694312be2c66e459ececbcc5789a6
Camya Robinson <camyacodes@gmail.com>
2024-08-18 19:12:40 -0400

    revised templates and subject line

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
-import React, { useState } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import CompanyNameInput from "./components/CompanyNameInput";
-import TemplateSearchInput from "./components/TemplateSearchInput";
-import TemplateDisplay from "./components/TemplateDisplay";
-import OutlookButton from "./components/OutlookButton";
-import { customizeTemplate } from "./components/utilities";
-import templates from "./templates.json";
+import React, { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import CompanyNameInput from './components/CompanyNameInput'
+import TemplateSearchInput from './components/TemplateSearchInput'
+import TemplateDisplay from './components/TemplateDisplay'
+import OutlookButton from './components/OutlookButton'
+import { customizeTemplate } from './components/utilities'
+import templates from './templates.json'
 
 function App() {
-  const [companyName, setCompanyName] = useState("");
-  const [searchTerm, setSearchTerm] = useState("");
-  const [selectedTemplate, setSelectedTemplate] = useState(null);
-  const [customizedText, setCustomizedText] = useState("");
-  const [emailBody, setEmailBody] = useState("");
+  const [companyName, setCompanyName] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+  const [selectedTemplate, setSelectedTemplate] = useState(null)
+  const [customizedText, setCustomizedText] = useState('')
+  const [emailBody, setEmailBody] = useState('')
 
   const handleTemplateSelection = (template) => {
     if (template) {
-      setSelectedTemplate(template);
-      setSearchTerm("");
+      setSelectedTemplate(template)
+      setSearchTerm('')
       const newText = companyName
         ? customizeTemplate(
-            template.body.replace("[COMPANY_NAME]", companyName),
+            template.body.replace('[COMPANY_NAME]', companyName),
             companyName
           )
-        : template.body;
-      setCustomizedText(newText);
-      setEmailBody(newText);
+        : template.body
+      setCustomizedText(newText)
+      setEmailBody(newText)
     }
-  };
+  }
 
   const clearTemplateSelection = () => {
-    setSelectedTemplate(null);
-    setCustomizedText("");
-    setEmailBody("");
-    setSearchTerm(""); // Optionally clear the search term as well
-  };
+    setSelectedTemplate(null)
+    setCustomizedText('')
+    setEmailBody('')
+    setSearchTerm('') // Optionally clear the search term as well
+  }
 
   const handleEmailBodyChange = (newText) => {
-    setEmailBody(newText);
-  };
+    setEmailBody(newText)
+  }
 
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
-      <div className="app-container shadow">
+      <div className='app-container shadow'>
         <CompanyNameInput
           companyName={companyName}
           setCompanyName={setCompanyName}
@@ -63,12 +63,12 @@ function App() {
           onTextChange={handleEmailBodyChange}
         />
         <OutlookButton
-          subject={`${companyName.toUpperCase()}`}
+          subject={`${companyName.toUpperCase()} - Credit Application`}
           body={emailBody}
         />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
